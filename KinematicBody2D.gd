abb742e3f053e66637b4806f8b1a6545c4b2a63f
Taber McFarlin <tabermcfarlin@gmail.com>
2019-04-13 01:23:58 -0400

    Smooth slopes

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -5,13 +5,14 @@ export var ground_speed: float = 500
 export var jump_height: float = 200
 export var jump_distance: float = 300
 export var max_slope_angle: float = 45
-export var jump_forgiveness: float = 0.05
+export var jump_forgiveness: float = 0.1
 
-# Physics constants
+# Movement constants
 var gravity: float
 var jump_speed: float
 
 var max_slope_x_normal: float
+var min_slope_y_normal: float
 
 # Input state
 var h_input: int = 0
@@ -28,13 +29,15 @@ var colY: KinematicCollision2D
 var colX: KinematicCollision2D
 
 var on_ground: bool = false
+var floor_normal: Vector2 = Vector2()
 var floor_velocity: Vector2 = Vector2()
 
 
 func _ready() -> void:
     gravity = 2 * jump_height * pow(ground_speed, 2) / pow(jump_distance / 2, 2)
     jump_speed = -2 * jump_height * ground_speed / (jump_distance / 2)
-    max_slope_x_normal = sin(deg2rad(max_slope_angle + .1))
+    max_slope_x_normal = sin(deg2rad(max_slope_angle + 1))
+    min_slope_y_normal = cos(deg2rad(max_slope_angle + 1))
 
 
 func get_input() -> void:
@@ -51,10 +54,23 @@ func get_input() -> void:
     jump_pressed = Input.is_action_just_pressed("jump")
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
     get_input()
+    
+    # Move
+    _movement(delta)
+    
 
-    # Movement Calculations------------------------------------
+func _movement(delta: float) -> void:
+    _calc_velocity(delta)
+    
+    colY = move_and_collide(Vector2(0, velocity.y) * delta)
+    colX = move_and_collide(Vector2(velocity.x, 0) * delta)
+    
+    _handle_collisions(delta)
+    
+
+func _calc_velocity(delta: float) -> void:
     # Horizontal Movement
     velocity.x = h_input * ground_speed + floor_velocity.x
 
@@ -66,29 +82,47 @@ func _process(delta: float) -> void:
     velocity.y += gravity * delta
     velocity.y = min(velocity.y, abs(jump_speed))
     jump_timer -= delta
-    #----------------------------------------------------------
 
-    # Move
-    colY = move_and_collide(Vector2(0, velocity.y) * delta)
-    colX = move_and_collide(Vector2(velocity.x, 0) * delta)
-    
-    # Process Collisions
+
+func _handle_collisions(delta: float) -> void:
+    # Vertical
     if abs(velocity.y) > 0 and colY:
         on_ground = velocity.y > 0
         velocity.y = 0
         
-        # Stick to platform going down
         if on_ground:
-            floor_velocity = colY.collider_velocity
-            velocity.y = max(floor_velocity.y, 0)
             jump_timer = jump_forgiveness
+            
+            # Stick to platform going down
+            floor_velocity = colY.collider_velocity
+            floor_normal = colY.normal
+            velocity.y = max(floor_velocity.y, 0)
     else:
         on_ground = false
-        floor_velocity = Vector2()
 
-    if velocity.x != 0 and colX:
-        if abs(colX.normal.x) < max_slope_x_normal and colX.normal.x != 0:
-            move_and_collide(Vector2(0, -(colX.normal.x / colX.normal.y) * colX.remainder.x))
-            move_and_collide(colX.remainder)
-        else:
-            velocity.x = 0
+    # Horizontal
+    if velocity.x != 0:
+        if colX:
+            # Collide with wall
+            if abs(colX.normal.x) > max_slope_x_normal:
+                velocity.x = 0
+            # Walk up slopes
+            else:
+                var y_off = -(colX.normal.x / colX.normal.y) * colX.remainder.x
+                move_and_collide(Vector2(0, y_off))
+                move_and_collide(colX.remainder)
+        # Check if walked off ledge/down slope
+        elif on_ground and colY and colY.remainder.y > 0:
+            colY = move_and_collide(colY.remainder)
+            
+            if not colY:
+                var slope_desc = abs(velocity.x) * delta / min_slope_y_normal
+                var should_stick = test_move(transform, Vector2(0, slope_desc))
+                
+                # Stick moving down slopes
+                if should_stick:
+                    move_and_collide(Vector2(0, abs(slope_desc)))
+                else:
+                    on_ground = false
+                
+                
